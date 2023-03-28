6c0fc5254b563aa86d50ced1ae31a208c461756b
dorra <youssefdorra@gmail.com>
2023-03-28 15:50:25 +0200

    update home screen with orientation b uilder

--- lib/activities/view/home.dart
+++ lib/activities/view/home.dart
@@ -7,90 +7,160 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    debugPrint(MediaQuery.of(context).orientation.name);
     return Scaffold(
-      body: Stack(
-        children: [
-          Image.asset(
-            "assets/background.png",
-            fit: BoxFit.cover,
-            width: MediaQuery.of(context).size.width,
-            height: double.infinity,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Expanded(
-                    child: Container(
-                  alignment: Alignment.center,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Expanded(
-                        flex: 5,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: const [
-                            Flexible(
-                              child: Text(
-                                'Hi! What do you want to do today?',
-                                style: TextStyle(fontSize: 26),
-                                textAlign: TextAlign.center,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      Expanded(
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Expanded(
-                              child: ElevatedButton(
-                                onPressed: () {
-                                  Navigator.of(context).push(
-                                    MaterialPageRoute(
-                                      builder: (context) =>
-                                      const RandomActivitiesList(),
-                                    ),
-                                  );
-                                },
-                                child: const Text("Random activities"),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      Expanded(
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Expanded(
-                              child: ElevatedButton(
-                                onPressed: () {
-                                  Navigator.of(context).push(
-                                    MaterialPageRoute(
-                                      builder: (context) =>
-                                      const ActivitySearch(),
-                                    ),
-                                  );
-                                },
-                                child: const Text("Find an activity"),
-                              ),
-                            ),
-                          ],
-                        ),
-                      )
-                     
-                    ],
-                  ),
-                )),
-                Expanded(child: Container())
-              ],
+      body: OrientationBuilder(
+        builder: (BuildContext context, Orientation orientation) => Stack(
+          children: [
+            Image.asset(
+              "assets/background.png",
+              fit: BoxFit.cover,
+              width: MediaQuery.of(context).size.width,
+              height: double.infinity,
             ),
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Expanded(
+                      child: Container(
+                    alignment: Alignment.center,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Expanded(
+                          flex: orientation == Orientation.portrait ? 3 : 5,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: const [
+                              Flexible(
+                                child: Text(
+                                  'Hi! What do you want to do today?',
+                                  style: TextStyle(fontSize: 26),
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Expanded(
+                          child: orientation == Orientation.portrait
+                              ? Column(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Expanded(
+                                      child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        children: [
+                                          Expanded(
+                                            child: ElevatedButton(
+                                              onPressed: () {
+                                                Navigator.of(context).push(
+                                                  MaterialPageRoute(
+                                                    builder: (context) =>
+                                                        const ActivitySearch(),
+                                                  ),
+                                                );
+                                              },
+                                              child: const Text(
+                                                  "Find an activity"),
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                    Expanded(
+                                      child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        children: [
+                                          Expanded(
+                                            child: ElevatedButton(
+                                              onPressed: () {
+                                                Navigator.of(context).push(
+                                                  MaterialPageRoute(
+                                                    builder: (context) =>
+                                                        const RandomActivitiesList(),
+                                                  ),
+                                                );
+                                              },
+                                              child: const Text(
+                                                  "Random activities"),
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ],
+                                )
+                              : Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: <Widget>[
+                                    Expanded(child: Container()),
+                                    Expanded(
+                                      flex: 2,
+                                      child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        children: [
+                                          Expanded(
+                                            child: ElevatedButton(
+                                              onPressed: () {
+                                                Navigator.of(context).push(
+                                                  MaterialPageRoute(
+                                                    builder: (context) =>
+                                                        const RandomActivitiesList(),
+                                                  ),
+                                                );
+                                              },
+                                              child: const Text(
+                                                  "Random activities"),
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                    Expanded(child: Container()),
+                                    Expanded(
+                                      flex: 2,
+                                      child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        children: [
+                                          Expanded(
+                                            child: ElevatedButton(
+                                              onPressed: () {
+                                                Navigator.of(context).push(
+                                                  MaterialPageRoute(
+                                                    builder: (context) =>
+                                                        const ActivitySearch(),
+                                                  ),
+                                                );
+                                              },
+                                              child: const Text(
+                                                  "Find an activity"),
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                    Expanded(child: Container()),
+                                  ],
+                                ),
+                        )
+                      ],
+                    ),
+                  )),
+                  Expanded(child: Container())
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
